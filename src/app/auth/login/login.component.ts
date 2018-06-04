1b3a5bb928e2c016294a6d81917a07ba919c7338
TNadir <nadir.tariverdiyev@gmail.com>
2018-06-04 23:48:11 +0400

    header completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -72,7 +72,7 @@ export class LoginComponent implements OnInit {
             this.message.text = '';
             window.localStorage.setItem('user', JSON.stringify(user));
             this.authService.login();
-
+            this.router.navigate(['/system','bill']);
 
           } else {
 
